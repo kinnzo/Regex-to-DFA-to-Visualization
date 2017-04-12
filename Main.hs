d59bd0a7359e8776013ebace6d17f5e0380449a0
Nikhil <tee15b014@iittp.ac.in>
2017-04-12 11:00:49 +0100

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -1,7 +1,6 @@
 import Language.HaLex.Dfa
 import Language.HaLex.FaAsDiGraph
---import Data.GraphViz
---import Data.GraphViz
+--This is the default type of Dfa is predefined in Haskell
 {-
 data Dfsa st sy  = Dfsa  [ sy ]  -- Vocabulary
             [ st ]  -- Finite set of states
@@ -9,8 +8,9 @@ data Dfsa st sy  = Dfsa  [ sy ]  -- Vocabulary
             [ st ]  -- The set of final states
           (st -> sy -> st)  -- The transition function
 -}
-ex1  :: Dfa Int Char
-ex1  = Dfa sym sta stt stf delta
+
+example  :: Dfa Int Char
+example  = Dfa sym sta stt stf delta
       where  
         delta 1 'a' = 1
         delta 1 'b' = 2
@@ -22,11 +22,9 @@ ex1  = Dfa sym sta stt stf delta
 	stf = [3]
 
 strig::String
-strig = dfa2DiGraphWithNoSyncSt ex1 "dot"
+strig = dfa2DiGraphWithNoSyncSt example "dot"
 
 main = do
   print (strig)
-  dfa2graphviz2file ex1 "out"
-  --runGraphvizCommand dot Char dfa2graphviz ex1 
-  --(Eq sy, Ord a, Show sy, Show a)
-  -- => Dfa a sy -> [Char] -> IO (
+  dfa2graphviz2file example "out"
+ 
